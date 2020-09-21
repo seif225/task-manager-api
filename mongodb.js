c130eb347e49d8ddc409faaa2c01b7173988c0d4
Seif ElDen Ehab <seif.eld.ehab@gmail.com>
2020-09-21 16:16:17 +0200

    first  commit

--- mongodb.js
+++ mongodb.js
@@ -1,5 +1,5 @@
-//CRUD create read update delete
-const mongodb = require('mongodb');
+//CRUD create read update delete  
+const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient;
 
 const connectionUrl = 'mongodb://127.0.0.1:27017';
